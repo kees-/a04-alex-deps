ed38457a1fe0235b7dde6631159103078d695756
kees- <6820950+kees-@users.noreply.github.com>
2022-05-03 10:10:11 -0700

    Increment some changes

--- src/clj/a04/core.clj
+++ src/clj/a04/core.clj
@@ -12,7 +12,10 @@
       :string
       [:map
        [:display {:optional true} string?]
-       [:contents vector?]
+       [:contents
+        [:map
+         [:type [:enum :nil :hiccup :text :link :img :video :audio :iframe]]
+         [:data any?]]]
        [:children [:ref ::node]]]]}}
    ::node])
 
@@ -41,6 +44,10 @@
    dissoc (last path)))
 
 ;; ========== SELECTION ========================================================
+(defn get-node
+  [tree path]
+  (->> (repeat :children) (interleave path) butlast (get-in tree)))
+
 (defn get-children
   "Get a map of children at a particular address."
   [tree path]
@@ -49,38 +56,66 @@
 ;; ========== TESTS ============================================================
 (def sample
   {"x" {:display "x"
-        :contents [:div "x content"]
-        :children {"a" {:display "a"
-                        :contents [:a "href"]
-                        :children {"m" {:display "m"
-                                        :contents [:div "m content"]
-                                        :children {}}
-                                   "n" {:display "n"
-                                        :contents [:div "n content"]
-                                        :children {}}}}
-                   "b" {:display "c"
-                        :contents []
-                        :children {}}}}
-   "y" {:display "y"
-        :contents [:div "y content"]
-        :children {"c" {:display "c"
-                        :contents []
-                        :children {}}}}})
+        :contents
+        {:type :hiccup
+         :data [:div "x content"]}
+        :children
+        {"a" {:contents
+              {:type :hiccup
+               :data [:a "href"]}
+              :children
+              {"m" {:contents
+                    {:type :hiccup
+                     :data [:div "m content"]}
+                    :children {}}
+               "n" {:contents
+                    {:type :hiccup
+                     :data [:div "n content"]}
+                    :children {}}}}
+         "b" {:display "c"
+              :contents
+              {:type :nil
+               :data nil}
+              :children {}}}}
+   "y" {:contents
+        {:type :hiccup
+         :data [:div "y content"]}
+        :children
+        {"c" {:contents {:type :nil
+                         :data nil}
+              :children {}}}}})
 
 (comment
  (malli/schema schema)
  (malli/properties schema)
  (malli/validate schema sample)
  (pprint sample)
- (pprint
-  (get-children sample ["x" "a"]))
+ (pprint (get-children sample ["x" "a"]))
+ (pprint (get-node sample ["x" "a"]))
+ (pprint (remove-node sample ["x" "a" "n"]))
  (pprint
   (add-node sample
             ["x"]
             (node {:address "a"
-                   :contents [:span "hello"]})))
- (pprint
-  (remove-node sample ["x" "a" "n"])))
+                   :contents [:span "hello"]}))))
+
+(comment
+ (let [a (node {:address "a" :contents [:div "goodmorning"]})
+       b (node {:address "b" :contents [:div "goodnight"]})
+       z (node {:address "z" :display "ℝ" :contents [:div "goodmorning"]})]
+   (-> (add-node {} [] a)
+       (add-node [] b)
+       (add-node ["a"] z)
+       pprint)))
+
+(comment
+ (malli/validate
+  schema
+  {"x" {:display "x"
+        :contents {:type :hiccup
+                   :data [:div "ee"]}
+        :children {}}}))
+
 ;; ========== EXEC =============================================================
 (defn -main
   "Do nothing"
